ce9fe78db5517bc4372ea95d920ff54ce0805b93
krishnazen27 <krishnazen27@gmail.com>
2021-03-19 08:05:56 +0530

    added single groovy

--- pipeline-defs/seedsinglejob.groovy
+++ pipeline-defs/seedsinglejob.groovy
@@ -1,4 +1,24 @@
-FreeStyleJob('Seed-single-Job') {
-    description('this job will seed a single job on the the current Jenkins env')
-
+freeStyleJob('Seed-Single-Job') { 
+    description('This job seeds a single job onto the current Jenkins env whilst leaving others untouched') 
+    logRotator(7,30) 
+    parameters { 
+        stringParam('PIPELINE_BRANCH', 'main', 'Name of branch in Pipelines repo') 
+        stringParam('PATH_TO_JOB', '', 'Pleaseeee do NOT use wildcards here') 
+    } 
+    scm { 
+        git { 
+            branch('${PIPELINE_BRANCH}') 
+            remote { 
+                url('https://github.com/krishnazen27/pipelines.git')
+                credentials('Github-Creds') 
+            } 
+        } 
+    }
+    
+    steps { 
+        dsl { 
+            external('${PATH_TO_JOB}') 
+            removeAction('IGNORE') 
+            } 
+        }
 }

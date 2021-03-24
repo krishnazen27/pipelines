6b55696887bad2e25a08d054450f51d4bd0b47a4
krishnazen27 <krishnazen27@gmail.com>
2021-03-24 08:29:34 +0530

    aa

--- pipeline-defs/job15.groovy
+++ pipeline-defs/job15.groovy
@@ -1,33 +1,15 @@
-pipelineJob('test3_Releases') { 
-    description("This job will only be used for regular release deployments to lower ORGS (DEV, QA, TEST, NFT)") 
-    parameters { 
-        string { 
-            name('BRANCH_NAME') 
-            defaultValue('master') 
-            description('The branch to be used from thebuild repo (case sensitive). Alternatively, use the commit ID. If left empty, dev branch will be picked') 
-            trim(true) 
-        } 
-        choiceParam('DEPLOYMENT_ENV', ['dev', 'qa', 'test', 'uat', 'nft'], 'Please select the environment(ORG) to trigger deployment. Default will be dev org') 
-        booleanParam { 
-            name('TEST_ONLY_VALIDATION') 
-            defaultValue(true) 
-            description('Enables to run Validations in seleced LOWER Env ')     
-        } 
-    } 
-    definition { 
-        cpsScm {
-            scm {
-                git {
-                    branch ('${BRANCH_NAME}')
-                    remote {
-                        credentials('')
-                        url('https://github.com/krishnazen27/Calculator.git')
-                    }
-                }
+multibranchPipelineJob("my_awesome_job") {
+    displayName "my awesome job"
+    description "multi-branch pipeline job thingy"
+    branchSources {
+        git {   
+            remote('https://github.com/krishnazen27/Calculator.git')
+            credentialsId('')
+            includes('*')
+            // set the default property strategy
+            defaultBranchPropertyStrategy {
+                noTriggerBranchProperty()
             }
-
-            scriptPath('script/jobsteps.groovy')
-
-        } 
+        }
     }
 }
